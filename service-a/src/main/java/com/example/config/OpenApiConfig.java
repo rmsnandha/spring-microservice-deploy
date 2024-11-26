a830bb5c4e3225bb79992b6c4378d16be6be7b70
rmsnandha <rmsnandha@example.com>
2024-11-26 16:01:05 -0600

    swagger

--- service-a/src/main/java/com/example/config/OpenApiConfig.java
+++ service-a/src/main/java/com/example/config/OpenApiConfig.java
@@ -1,33 +1,19 @@
 package com.example.config;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
-import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 
 @Configuration
 public class OpenApiConfig {
 
     @Bean
-    public OpenAPI customOpenAPI() {
+    public OpenAPI usersMicroserviceOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("Library Management System API")
-                        .version("1.0")
-                        .description("API documentation for the Library Management System.")
-                        .termsOfService("http://swagger.io/terms/")
-                        .contact(new Contact().name("API Support").email("support@example.com").url("http://example.com"))
-                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
-    }
-
-    @Bean
-    public GroupedOpenApi publicApi() {
-        return GroupedOpenApi.builder()
-                .group("public")
-                .pathsToMatch("/api/**")
-                .build();
+                        .title("Your API Title")
+                        .description("Your API Description")
+                        .version("1.0"));
     }
 }
