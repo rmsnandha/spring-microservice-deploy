cb9f71ff99d3123da3978eb8ec5a03f50bcb46fa
rmsnandha <rmsnandha@example.com>
2024-12-06 09:08:30 -0600

    logback and splunk

--- service-a/src/main/java/com/example/controller/Service1To2Controller.java
+++ service-a/src/main/java/com/example/controller/Service1To2Controller.java
@@ -1,5 +1,6 @@
 package com.example.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,12 +8,14 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 
 @RestController
+@Slf4j
 public class Service1To2Controller {
 
     @Autowired private WebClient service2WebClient;
 
      @GetMapping("/service1to2")
     String service1to2() {
+         log.info("Calling service 2 from service 1");
         return service2WebClient.get()
             .uri("/hello")
             .retrieve()
